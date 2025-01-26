0db667ea9a85a545d884c47c06ce3d89c14f8696
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2025-01-25 22:59:28 -0300

    fetching data using TMDB API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,35 @@
-import Image from "next/image";
+import { MovieProps } from '@/utils/movie.type'
 import styles from "./page.module.scss";
+import Link from 'next/link';
 
-export default function Home() {
-  return (
-    <div >
-       page.tsx
+async function getFilmes() {
+  try{
+    const res = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en&page=1`, { next: { revalidate: 320 } })
+    const data = await res.json() 
+    const movies = data.results.slice(0, 12)
+    // console.log(movies)
+    return movies
+  }catch(err){
+    throw new Error("Failed to fetch data")
+  }
+} 
+
+export default async function Home(){
+  const movies: MovieProps[] = await getFilmes()
+
+  return(
+    <div>
+      <div className={styles.containerMovies}>
+        {movies.map((movie) => {
+          return(
+            <article key={movie.id}>
+              <strong>{movie.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt={movie.title} />
+              <Link href={`/movie/${movie.id}`}>View Details</Link>
+            </article>
+          )
+        })}
+      </div>
     </div>
-  );
+  )
 }
---
+++ src/utils/movie.type.ts
@@ -0,0 +1,6 @@
+
+export interface MovieProps{
+    id: number;
+    title: string;
+    backdrop_path: string;
+}
