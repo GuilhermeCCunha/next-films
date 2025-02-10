8890c4c783839c9d8f4074037c6ef4a7e38ec066
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2025-02-10 13:20:32 -0300

    setting and getting movies in localStorage

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,17 @@
+import { MovieDetailsProps } from "./types/details";
+
 export const calcTime = (time: number): string => {
   const hours: number = Math.floor(time / 60);
   const mins: number = time % 60;
   return `${hours}h ${mins}m`;
+}
+
+export const getsavedMovies = (): Array<MovieDetailsProps>  => { 
+  const savedMovies: string = localStorage.getItem("nextfilms") || '[]';
+  let movies: Array<MovieDetailsProps>
+  return movies = JSON.parse(savedMovies) || [];
+}
+
+export const setSavedMovies = (movies: MovieDetailsProps[]) => {
+  localStorage.setItem("nextfilms", JSON.stringify(movies))
 }
