299ce776d64c2aa8b2f07b1380566352470b55d1
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2025-02-26 12:14:06 -0300

    searching for movies

--- src/utils/get-data.ts
+++ src/utils/get-data.ts
@@ -24,3 +24,15 @@ export async function getMovieDetails(id: string) {
 
   return movie  
 }
+
+export async function searchMovies(name: string) {
+  try{
+    const res = await fetch(`${API_URL}/search/movie?api_key=${API_KEY}&query=${name}&language=en&page=1`, { next: { revalidate: 320 } })
+    const data = await res.json() 
+    const movies = data.results
+      // console.log(movies)
+    return movies
+  }catch(err){
+    throw new Error("Failed to fetch data")
+  }
+}
