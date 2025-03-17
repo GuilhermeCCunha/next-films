dc55705faec1aa0e86fd90ab082bcd4e51bc3556
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2025-03-16 22:22:08 -0300

    load more (infinite scroll)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,24 @@ import { MovieProps } from '@/utils/types/movie'
 import styles from "./page.module.scss";
 import { getMovies } from '@/utils/get-data';
 import MovieCard from '@/components/moviecard';
+import LoadMore from '@/components/loadmore';
 
 export default async function Home(){
   const movieprops: MovieProps = await getMovies(1)
   const movies: MovieProps['results'] = movieprops.results
+  const totalPages: MovieProps['total_pages'] = movieprops.total_pages
 
-  return(
-    <div>
-      <div className={styles.moviesContainer}>
-        {movies.map((movie) => {
-          return(
-            <MovieCard key={movie.id} id={movie.id} title={movie.title} img={movie.backdrop_path} />
-          )
-        })}
-      </div>
+  return (
+    <div className={styles.moviesContainer}>
+      {movies.map((movie) => {
+        return (
+          <MovieCard key={movie.id} id={movie.id} title={movie.title} img={movie.backdrop_path} />
+        )
+      })}
+      {
+        totalPages > 1 &&
+        <LoadMore />
+      }
     </div>
   )
 }
---
+++ src/components/loadmore/index.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from "react";
+import { getMovies } from "@/utils/get-data";
+import { MovieProps } from "@/utils/types/movie";
+import MovieCard from "../moviecard";
+
+export default function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<MovieProps['results']>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [page, setPage] = useState<number>(2)
+  const [totalPages, setTotalPages] = useState<MovieProps['total_pages']>(3)
+  const [completed, setCompleted] = useState<boolean>(false)
+  
+  useEffect(() => {
+    setCompleted(false);
+    setData([]);
+    setPage(2);
+    getMovies(page).then((res) => {
+      setTotalPages(res?.total_pages);
+    });
+  }, []) 
+
+  useEffect(() => {
+    if (inView) {
+      setIsLoading(true);
+      if (page > totalPages) {
+        setCompleted(true);
+      }
+      const delay = 1850;
+
+      const timeoutId = setTimeout(() => {
+        getMovies(page).then((res) => {
+          let movies = res.results
+          setData([...data, ...movies]);
+          setPage(page + 1);
+        });
+
+        setIsLoading(false);
+      }, delay);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [inView, data, isLoading]);
+
+  return (
+    <>
+      <section>
+        {data.map((movie) => {
+          return (
+            <MovieCard key={movie.id} id={movie.id} title={movie.title} img={movie.backdrop_path} />
+          )
+        })}
+      </section>
+      <section>
+        {!completed ?
+          <div ref={ref}>
+            {inView && isLoading && (
+              <p>Loading...</p>
+            )}
+          </div>
+          : <p>No more movies to load</p>
+        } 
+      </section>
+    </>
+  );
+}
