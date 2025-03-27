e12114b471dbe937731eb62ea1ed7e7c83e95fc9
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2025-03-26 22:18:01 -0300

    getting movies by genre

--- src/utils/get-data.ts
+++ src/utils/get-data.ts
@@ -32,3 +32,15 @@ export async function searchMovies(page: number, name: string) {
     throw new Error("Failed to fetch data")
   }
 }
+
+export async function getMoviesByGenre(genreId: number) {
+  try{
+    const res = await fetch(`${API_URL}/discover/movie?api_key=${API_KEY}&with_genres=${genreId}&language=en&page=1`, { next: { revalidate: 320 } })
+    const data = await res.json() 
+    const movies = data.results.slice(0, 12)
+      // console.log(data)
+    return movies
+  }catch(err){
+    throw new Error("Failed to fetch data")
+  }
+}
