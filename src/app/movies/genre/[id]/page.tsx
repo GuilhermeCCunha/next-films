b8da6df548e6b7cb62a0e036c8fc6a48dd86a514
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2025-03-30 16:37:06 -0300

    movies by genre page

---
+++ src/app/movies/genre/[id]/page.tsx
@@ -0,0 +1,32 @@
+import { MovieProps } from '@/utils/types/movie'
+import { getGenreList, getMoviesByGenre } from '@/utils/get-data';
+import MovieCard from '@/components/moviecard';
+import { Genre } from '@/utils/types/details';
+import { notFound } from 'next/navigation'
+
+export async function generateMetadata(props: { params: Promise<{ id: string }> }) {
+  const genrelist: Genre[] = await getGenreList()
+  const { id } = await props.params; 
+  let genre = genrelist.find(genre => genre.id === parseInt(id));
+
+  return {
+    title: `${genre?.name} | Genre`,
+  };
+}
+
+export default async function MovieGenre(props: { params: Promise<{ id: string }> }) {
+
+  const { id } = await props.params;
+  const movies: MovieProps['results'] = await getMoviesByGenre(parseInt(id))
+
+  return (
+    <div>
+      {movies?.length === 0 && notFound()}
+      {movies.map((movie) => {
+        return (
+          <MovieCard key={movie.id} id={movie.id} title={movie.title} img={movie.backdrop_path} />
+        )
+      })}
+    </div>
+  )
+}
