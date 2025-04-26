a1a76e624957b36ae45effeb3f23728bfc24606d
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2025-04-25 21:57:25 -0300

    styling the sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,25 +1,39 @@
 "use client";
 import Link from "next/link";
+import styles from "./styles.module.scss";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 import { useGenresContext } from "@/context/genrescontext";
 
 export default function SideBar() {
   const [isOpen, setIsOpen] = useState<boolean>(true);
   const { genreList } = useGenresContext();
-
+  const pathname = usePathname();
+  
   return (
     <>
-      <button onClick={() => setIsOpen(!isOpen)}>&nbsp; Genres &nbsp;</button>
-      <div>
+      <button
+        className={`${styles.sidebarButton} ${isOpen && styles.open}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        &nbsp; Genres &nbsp;
+      </button>
+      <div className={`${styles.sidebar} ${!isOpen && styles.hiddenSidebar}`}>
         {genreList.map((genre) => {
           let href = `/movies/genre/${genre.id}`;
           return (
-            <Link key={genre.id} href={href}>
+            <Link
+              className={`${pathname === href && styles.active}`}
+              key={genre.id}
+              href={href}
+            >
               {genre.name}
             </Link>
           );
         })}
-        <Link href="/">Home</Link>
+        <Link className={`${pathname === "/" && styles.active}`} href="/">
+          Home
+        </Link>
       </div>
     </>
   );
