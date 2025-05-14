193202da9da9997363e18231bceada92655a3657
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2025-05-13 22:52:51 -0300

    standardizing code formatting

--- src/utils/types/details.ts
+++ src/utils/types/details.ts
@@ -1,14 +1,14 @@
 import { Movie } from "./movie";
 
 export interface MovieDetailsProps extends Movie {
-    genres: Genre[];
-    overview: string;
-    vote_average: number;
-    release_date: string;
-    runtime: number; 
+  genres: Genre[];
+  overview: string;
+  vote_average: number;
+  release_date: string;
+  runtime: number;
 }
 
 export interface Genre {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
--- src/utils/types/movie.ts
+++ src/utils/types/movie.ts
@@ -1,12 +1,12 @@
 
-export interface MovieProps{
-    results: Movie[]
-    total_pages: number
+export interface MovieProps {
+  results: Movie[]
+  total_pages: number
 }
 
 export interface Movie {
-    id: number;
-    title: string;
-    backdrop_path: string;       
-    poster_path: string;         
+  id: number;
+  title: string;
+  backdrop_path: string;
+  poster_path: string;
 }
